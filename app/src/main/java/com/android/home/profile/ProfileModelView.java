869eecbacf6202c11003392fcfc68f61dca4f24a
Bin Bin <phuduthang300502@gmail.com>
2024-04-15 19:11:43 +0700

    PROFILE PAGE

--- app/src/main/java/com/android/home/profile/ProfileModelView.java
+++ app/src/main/java/com/android/home/profile/ProfileModelView.java
@@ -1,9 +1,56 @@
 package com.android.home.profile;
 
+import android.view.View;
+
 import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import com.android.BR;
 
 public class ProfileModelView extends BaseObservable {
+    private String fullName, phone, email;
+
     public ProfileModelView() {
 
+    }
+
+    public ProfileModelView(String fullName, String phone, String email) {
+        this.fullName = fullName;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    @Bindable
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+        notifyPropertyChanged(BR.fullName);
+    }
+
+    @Bindable
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+        notifyPropertyChanged(BR.phone);
+    }
+
+    @Bindable
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+        notifyPropertyChanged(BR.email);
+    }
+
+    public void updateProfile(View view) {
+        // CALL API UPDATE PROFILE.
     }
 }
