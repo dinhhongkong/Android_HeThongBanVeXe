380d1df6b989075ca6a6e4f5c90f71e3450dd4fc
DinhHongKong <83532836+dinhhongkong@users.noreply.github.com>
2024-05-15 20:32:41 +0700

    edit layouts

--- app/src/main/java/com/android/feature/login/LoginFragment.java
+++ app/src/main/java/com/android/feature/login/LoginFragment.java
@@ -6,8 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import com.android.R;
 import com.android.databinding.FragmentLoginBinding;
 
 public class LoginFragment extends Fragment {
@@ -18,6 +21,14 @@ public class LoginFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentLoginBinding.inflate(inflater,container,false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.btnLogin.setOnClickListener(v->{
+            Navigation.findNavController(requireView()).navigate(R.id.action_loginFragment_to_homeFragment);
+        });
     }
 
     @Override
